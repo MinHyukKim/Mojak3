a05c7ead5c36643867baa8926e8e9d2c80b596d6
NOTEBOOK\MinHyuk <rlaalsgur05@naver.com>
2017-09-02 22:24:33 +0900

    cFont 구현 (테스트 안됨)

--- DirectX_Frame/DirectX_Frame/cFont.cpp
+++ DirectX_Frame/DirectX_Frame/cFont.cpp
@@ -2,48 +2,60 @@
 #include "cFont.h"
 
 cFont::cFont(void)
+	: m_pFont(NULL)
+	, m_dwColor(D3DCOLOR_XRGB(255, 255, 255))		//글씨 기본 색상
+	, m_dwFomat(DT_LEFT | DT_TOP)					//글씨 LeftTop 정렬
 {
 }
 
 cFont::~cFont(void)
 {
+	SAFE_RELEASE(m_pFont);							//폰트 릴리즈
 }
 
 
-void cFont::Setup(void)
+void cFont::Setup(int nFontSize, LPCSTR szFontStyle)
 {
-	SAFE_RELEASE(m_pFont);
+	SAFE_RELEASE(m_pFont);							//폰트가 있으면 Release
+
 	D3DXFONT_DESC fd;
-	ZeroMemory(&fd, sizeof(D3DXFONT_DESC));
-	fd.Height = FONT_SIZE;
-	fd.Width = FONT_SIZE / 2;
+	ZeroMemory(&fd, sizeof(D3DXFONT_DESC));			//지역변수 초기화
+	fd.Height = nFontSize;							//폰트 크기
+	fd.Width = nFontSize / 2;						//폰트 크기의 절반
 	fd.Weight = FW_NORMAL;
 	fd.Italic = false;
 	fd.CharSet = DEFAULT_CHARSET;
 	fd.OutputPrecision = OUT_DEFAULT_PRECIS;
 	fd.PitchAndFamily = FF_DONTCARE;
 
-	lstrcpyn(fd.FaceName, FONT_STYLE, FONT_LENGTH);					//폰트 타입 설정
-	D3DXCreateFontIndirect(g_pD3DDevice, &fd, &m_pFont);			//폰트 생성
-	SetRect(&m_rc, 0, 0, FONT_SIZE / 2 * FONT_LENGTH, FONT_SIZE);	//글씨 출력 범위
-	m_dwColor = D3DCOLOR_XRGB(255, 255, 255);						//글씨 기본 색상
-	m_dwFomat = DT_LEFT | DT_TOP;									//글씨 LeftTop 정렬
+	lstrcpyn(fd.FaceName, szFontStyle, LF_FACESIZE);										//폰트 스타일 이름(Buffer 있음)
+	D3DXCreateFontIndirect(g_pD3DDevice, &fd, &m_pFont);									//폰트 생성
+	if (nFontSize)																			// if(0 == fontsize) 출력범위 수정안함
+	{
+		m_nFontSize = nFontSize;
+		if (m_text.empty) this->SetRectangle(0, 0, fd.Width * FONT_LENGTH / 2, nFontSize);	//글씨가 없으면 기본값 = 32
+	}
 }
 
-void cFont::Setup(int x, int y, LPCSTR text, int size)
+void cFont::DrawFont(int nX, int nY, LPCSTR szText)
 {
+	int nStrlen = strlen(szText);															//글자수 찾기
+	this->SetRectangle(nX, nY, m_nFontSize * nStrlen / 2 + 1, m_nFontSize);					//상자 이동
+	this->SetText(szText);																	//텍스트 내용 수정
 }
 
 void cFont::Render(void)
 {
+	//폰트 출력
+	if (m_pFont) m_pFont->DrawText(NULL, m_text.c_str(), m_text.length(), &m_rc, m_dwFomat, m_dwColor);
 }
 
-cFont * cFont::Create(void)
+cFont* cFont::Create(void)
 {
 	return nullptr;
 }
 
-cFont * cFont::AddRef(void)
+cFont* cFont::AddRef(void)
 {
 	return nullptr;
 }
--- DirectX_Frame/DirectX_Frame/cFont.h
+++ DirectX_Frame/DirectX_Frame/cFont.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#define FONT_SIZE 20
-#define FONT_LENGTH 32
 #define FONT_STYLE "궁서체"
+#define FONT_LENGTH 32
 
 class cFont : public cObject
 {
@@ -13,15 +12,20 @@ private:
 	RECT	m_rc;
 	DWORD	m_dwFomat;
 	DWORD	m_dwColor;
+	int		m_nFontSize;
 
 public:
-	//기본 설정 (Setup)
-	void Setup(void);
-	void Setup(int x, int y, LPCSTR text, int size = 20);
+	//기본 설정 및 수정 (Setup) 글씨크기나 스타일 변경시 사용
+	void Setup(int nFontSize = 20, LPCSTR szFontStyle = FONT_STYLE);
 
 	//정보 수정 (Update)
+	void SetRectangle(int nX, int nY, int nWidth, int nHeight) { SetRect(&m_rc, nX, nY, nX + nWidth, nY + nHeight); }
+	void SetText(LPCSTR szText) { m_text = szText; }			//글씨 내용
+	void DrawFont(int nX, int nY, LPCSTR szText);				//글씨 위치와 내용
+	void SetFomat(DWORD dwFomat) { m_dwFomat = dwFomat; }		//글씨 정렬 설정
+	void SetColor(DWORD dwColor) { m_dwColor = dwColor; }		//글씨 색상 설정
 
-	//랜더
+	//실제 출력 타이밍 (Render)
 	void Render(void);
 
 
