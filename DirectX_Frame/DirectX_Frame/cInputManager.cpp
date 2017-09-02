6b6f3f1994fe8ea4ec7e5597870386c3bb360160
DESKTOP-EBRN4UM\Tester <lazedays@naver.com>
2017-09-02 18:42:14 +0900

    cInputManager 키 bool 함수 껍데기 생성

--- DirectX_Frame/DirectX_Frame/cInputManager.cpp
+++ DirectX_Frame/DirectX_Frame/cInputManager.cpp
@@ -1,6 +1,26 @@
 #include "stdafx.h"
 #include "cInputManager.h"
 
+bool cInputManager::IsOnceKeyDown(const int nKey)
+{
+	return false;
+}
+
+bool cInputManager::IsOnceKeyUp(const int nKey)
+{
+	return false;
+}
+
+bool cInputManager::IsStayKeyDown(const int nKey)
+{
+	return false;
+}
+
+bool cInputManager::IsToggleKey(const int nKey)
+{
+	return false;
+}
+
 cInputManager::cInputManager(void)
 {
 
