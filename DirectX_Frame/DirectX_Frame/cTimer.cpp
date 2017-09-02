df4ff40e25d99ce43c54d25a29a2ae4b9cf2b2a9
cafealpha <cyanine@naver.com>
2017-09-03 05:44:26 +0900

    Setup함수의 타이머 설정 작성

--- DirectX_Frame/DirectX_Frame/cTimer.cpp
+++ DirectX_Frame/DirectX_Frame/cTimer.cpp
@@ -15,6 +15,27 @@ void cTimer::Setup(void)
 {
 	//프레임 해제
 	this->SetLockFPS();
+
+	//하드웨어 정밀 타이머를 사용할 수 있을 경우
+	if (QueryPerformanceFrequency((LARGE_INTEGER*)&_periodFrequency))
+	{
+		//하드웨어 타이머가 사용가능하다는것을 세트
+		_isHardware = true;
+		//초기 시간 설정
+		QueryPerformanceCounter((LARGE_INTEGER*)&_lastTime));
+		//초당 빈도수 설정
+		_timeScale = 1.0f / _periodFrequency;
+	}//하드웨어 정밀 타이머를 사용할 수 없을 경우
+	else
+	{
+		//하드웨어 타이머 사용 불가 세트
+		_isHardware = false;
+		//초기 시간을 getTickCount에서 받아온다
+		_lastTime = GetTickCount(); 
+		//타임스케일은 0.001f로 설정
+		_timeScale = 0.001f;
+	}
+
 }
 
 void cTimer::Update(void)
