2a381bbde569dc5beedd458c609b622505b396a8
DESKTOP-EBRN4UM\Tester <lazedays@naver.com>
2017-09-04 18:03:47 +0900

    cSceneObject 클레스 생성

--- DirectX_Frame/DirectX_Frame/cSceneManager.h
+++ DirectX_Frame/DirectX_Frame/cSceneManager.h
@@ -1,7 +1,15 @@
 #pragma once
 class cSceneManager
 {
+private:
+
 public:
+
+
+	//½Ì±ÛÅæ »ı¼º
+	static cSceneManager* GetInstance() { static cSceneManager instance; return &instance; }
+
+private:
 	cSceneManager(void);
 	virtual ~cSceneManager(void);
 };
