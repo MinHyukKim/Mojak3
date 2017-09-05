6bbb033e45deb5350e8ca3cd78d1f763ee30f228
NOTEBOOK\MinHyuk <rlaalsgur05@naver.com>
2017-09-05 18:16:51 +0900

    카메라 구현중...

--- DirectX_Frame/DirectX_Frame/cCamera.cpp
+++ DirectX_Frame/DirectX_Frame/cCamera.cpp
@@ -2,6 +2,12 @@
 #include "cCamera.h"
 
 cCamera::cCamera(void)
+	: m_eCameraType(cCamera::E_LANDOBJECT)
+	, m_vAxisX(1.0f, 0.0f, 0.0f)
+	, m_vAxisY(0.0f, 1.0f, 0.0f)
+	, m_vAxisZ(0.0f, 0.0f, 1.0f)
+	, m_vPosition(0.0f, 0.0f, -1.0f)
+	, m_pParentMatrix(NULL)
 {
 }
 
@@ -9,4 +15,141 @@ cCamera::~cCamera(void)
 {
 }
 
+void cCamera::Setup(void)
+{
+
+}
+
+void cCamera::Update(void)
+{
+}
+
+void cCamera::SetupParentMatrix(IN LPD3DXMATRIX pWorldMatrix)
+{
+	if (m_pParentMatrix && !pWorldMatrix)
+	{
+		m_vPosition = m_vPosition + D3DXVECTOR3(m_pParentMatrix->_41, m_pParentMatrix->_42, m_pParentMatrix->_43);
+		D3DXVec3Normalize(&m_vAxisX, &(m_vAxisX + D3DXVECTOR3(m_pParentMatrix->_11, m_pParentMatrix->_12, m_pParentMatrix->_13)));
+		D3DXVec3Normalize(&m_vAxisY, &(m_vAxisY + D3DXVECTOR3(m_pParentMatrix->_21, m_pParentMatrix->_22, m_pParentMatrix->_23)));
+		D3DXVec3Normalize(&m_vAxisZ, &(m_vAxisZ + D3DXVECTOR3(m_pParentMatrix->_31, m_pParentMatrix->_32, m_pParentMatrix->_33)));
+	}
+	else if (!m_pParentMatrix && pWorldMatrix)
+	{
+		m_vPosition = D3DXVECTOR3(0.0f, 0.0f, -1.0f);
+		m_vAxisY = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+		m_vAxisZ = D3DXVECTOR3(0.0f, 0.0f, 1.0f);
+	}
+
+	this->SetParentMatrix(pWorldMatrix);
+}
+
+void cCamera::MovePositionX(OUT float fUnits)
+{
+	if (!m_pParentMatrix) return;
+
+	m_vPosition.x += fUnits;
+}
+
+void cCamera::MovePositionY(OUT float fUnits)
+{
+	if (!m_pParentMatrix) return;
+
+	m_vPosition.y += fUnits;
+}
+
+void cCamera::MovePositionZ(OUT float fUnits)
+{
+	m_vPosition.z += fUnits;
+}
+
+void cCamera::AxisDirectionX(OUT float fAngle)
+{
+	D3DXMATRIXA16 matR;
+	D3DXMatrixRotationAxis(&matR, &m_vAxisX, fAngle);
+	D3DXVec3TransformCoord(&m_vAxisY, &m_vAxisY, &matR);
+	D3DXVec3TransformCoord(&m_vAxisZ, &m_vAxisZ, &matR);
+}
+
+void cCamera::AxisDirectionY(OUT float fAngle)
+{
+	D3DXMATRIXA16 matR;
+	switch (m_eCameraType)
+	{
+	case cCamera::E_LANDOBJECT: D3DXMatrixRotationY(&matR, fAngle); break;
+	case cCamera::E_AIRCRAFT: D3DXMatrixRotationAxis(&matR, &m_vAxisY, fAngle); break;
+	default: return;
+	}
+	D3DXVec3TransformCoord(&m_vAxisX, &m_vAxisX, &matR);
+	D3DXVec3TransformCoord(&m_vAxisZ, &m_vAxisZ, &matR);
+}
+
+void cCamera::AxisDirectionZ(OUT float fAngle)
+{
+	if (E_AIRCRAFT)
+	{
+		D3DXMATRIXA16 matR;
+		D3DXMatrixRotationAxis(&matR, &m_vAxisZ, fAngle);
+		D3DXVec3TransformCoord(&m_vAxisX, &m_vAxisX, &matR);
+		D3DXVec3TransformCoord(&m_vAxisY, &m_vAxisY, &matR);
+	}
+}
+
+void cCamera::UpdateViewSpace(void)
+{
+
+	if (cCamera::E_LANDOBJECT == m_eCameraType)
+	{
+		if (m_pParentMatrix)
+		{
+			//고정 시점
+			D3DXVECTOR3 vParentPosition(m_pParentMatrix->_41, m_pParentMatrix->_42, m_pParentMatrix->_43);
+			D3DXVECTOR3 vParentAxisY(m_pParentMatrix->_31, m_pParentMatrix->_32, m_pParentMatrix->_33);
+			D3DXMatrixLookAtLH(&m_matViewMatrix, &(vParentPosition + m_vPosition), &(vParentAxisY - m_vAxisZ), &D3DXVECTOR3(0.0f, 1.0f, 0.0f));
+		}
+		else
+		{
+			//자유 시점
+			D3DXMatrixLookAtLH(&m_matViewMatrix, &m_vPosition, &(-m_vAxisZ), &D3DXVECTOR3(0.0f, 1.0f, 0.0f));
+		}
+	}
+	else if (cCamera::E_AIRCRAFT == m_eCameraType)
+	{
+		if (m_pParentMatrix)
+		{
+			//고정 시점
+			D3DXVECTOR3 vParentPosition(m_pParentMatrix->_41, m_pParentMatrix->_42, m_pParentMatrix->_43);
+			D3DXVECTOR3 vParentAxisY(m_pParentMatrix->_31, m_pParentMatrix->_32, m_pParentMatrix->_33);
+			D3DXMatrixLookAtLH(&m_matViewMatrix, &(vParentPosition + m_vPosition), &(vParentAxisY - m_vAxisZ), &m_vAxisY);
+		}
+		else
+		{
+			//자유 시점
+			D3DXMatrixLookAtLH(&m_matViewMatrix, &m_vPosition, &(-m_vAxisZ), &m_vAxisY);
+		}
+	}
+
+	g_pD3DDevice->SetTransform(D3DTS_VIEW, &m_matViewMatrix);
+}
+
+void cCamera::UpdateViewSpace(IN const CAMERA_TYPE eCameraType)
+{
+	m_eCameraType = eCameraType;
+	this->UpdateViewSpace();
+}
+
+void cCamera::UpdateProjection(IN float fMinZ, IN float fMaxZ, IN float fAngle)
+{
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	D3DXMATRIXA16 matProj;
+	D3DXMatrixPerspectiveFovLH(&matProj, fAngle, (float)rc.right / (float)rc.bottom, fMinZ, fMaxZ);
+	g_pD3DDevice->SetTransform(D3DTS_PROJECTION, &matProj);
+}
+
+cCamera* cCamera::Create(void)
+{
+	cCamera* newClass = new cCamera;
+	newClass->AddRef();
+	return newClass;
+}
 
--- DirectX_Frame/DirectX_Frame/cCamera.h
+++ DirectX_Frame/DirectX_Frame/cCamera.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "cObject.h"
 
+//용책 275p 참고
 class cCamera : public cObject
 {
 public:
-	//용책 275p 참고
 	enum CAMERA_TYPE
 	{
 		E_LANDOBJECT,
@@ -12,29 +12,52 @@ public:
 	};
 
 private:
+	CAMERA_TYPE m_eCameraType;
 
-	static RECT m_rc;
-	//카메라 거리
-	float m_fDistance;
+	D3DXVECTOR3 m_vAxisX;
+	D3DXVECTOR3 m_vAxisY;
+	D3DXVECTOR3 m_vAxisZ;
+	D3DXVECTOR3 m_vPosition;
+
+	D3DXMATRIXA16 m_matViewMatrix;
+
+	LPD3DXMATRIX m_pParentMatrix;
 
 public:
-//	virtual void Setup(void) override;
-//	virtual void Update(void) override;
-//	virtual void Render(void) override;
-	
-	//카메라 시점 설정
-//	virtual void SetView(void) final;
-	//입력기에 의한 카메라 시점 변경을 받아주는 함수
-//	virtual void SetControl(void);
-	//카메라가 보는 방향에서 쏘는 광선
-//	virtual void GetRayDirection(OUT D3DXVECTOR3* vPosition, OUT D3DXVECTOR3* vDirection, POINT& ptPosition);
-	//카메라 시야각 설정 함수
-	static void SetFovLH(void);
+	void Setup(void);
+	void Update(void);
 
+	//부모 매트릭스 설정
+	void SetupParentMatrix(IN LPD3DXMATRIX pWorldMatrix);
+	void SetParentMatrix(IN LPD3DXMATRIX pWorldMatrix) { m_pParentMatrix = pWorldMatrix; }
+	LPD3DXMATRIX GetParentMatrix(void) { return m_pParentMatrix; }
+	void SetPosition(IN LPD3DXVECTOR3 pPosition) { m_vPosition = *pPosition; }
+
+	//상대 이동
+	void MovePositionX(OUT float fUnits);						// 좌/우 이동
+	void MovePositionY(OUT float fUnits);						// 상/하 이동
+	void MovePositionZ(OUT float fUnits);						// 앞/뒤 이동
+
+	//상대 회전
+	void AxisDirectionX(OUT float fAngle);						// 상/하 회전
+	void AxisDirectionY(OUT float fAngle);						// 좌/우 회전
+	void AxisDirectionZ(OUT float fAngle);						// 뱅기용 회전
+
+	void SetCameraType(IN CAMERA_TYPE eCameraType) { m_eCameraType = eCameraType; }
+	LPD3DXMATRIX GetViewMatrix(void) { return &m_matViewMatrix; }
+
+	LPD3DXVECTOR3 GetPosition(void) { return &m_vPosition; }
+	LPD3DXVECTOR3 GetAxisX(void) { return &m_vAxisX; }
+	LPD3DXVECTOR3 GetAxisY(void) { return &m_vAxisY; }
+	LPD3DXVECTOR3 GetAxisZ(void) { return &m_vAxisZ; }
+
+	void UpdateViewSpace(IN const CAMERA_TYPE eCameraType);
+	void UpdateViewSpace(void);
+
+	static void UpdateProjection(IN float fMinZ = 1.0f, IN float fMaxZ = 1000.0f, IN float fAngle = D3DX_PI / 4.0f);
+	
 	//카메라 생성 함수
 	static cCamera* Create(void);
-
-
 protected:
 	cCamera(void);
 	virtual ~cCamera(void);
