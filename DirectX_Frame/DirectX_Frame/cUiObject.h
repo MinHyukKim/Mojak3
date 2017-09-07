5f17746c548d3bf3e84d2a9415ca978ccec4a8f9
DESKTOP-EBRN4UM\Tester <lazedays@naver.com>
2017-09-08 02:28:58 +0900

    cUiObject 진행중

--- DirectX_Frame/DirectX_Frame/cUiObject.h
+++ DirectX_Frame/DirectX_Frame/cUiObject.h
@@ -13,9 +13,14 @@ protected:
 	SYNTHESIZE(int, m_nTag, Tag);
 
 public:
-	virtual HRESULT Setup(void) override;
+	virtual HRESULT Setup(void) override;	
 	virtual void Update(void) override;
 	virtual void Render(void) override;
+	void AddChild(cUiObject* pChild);
+	void Render(LPD3DXSPRITE pSprite);
+	void SetPosition(float x, float y);
+	cUiObject* GetChildByTag(int nTag);
+
 
 	// 오브젝트를 상속받은 모든 클래스는 아래와 같은 형식을 사용합니다. (오버로딩하여 사용하세요.)
 	static cUiObject* Create(void);		//메모리관리용 생성자 
