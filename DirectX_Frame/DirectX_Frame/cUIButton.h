83fbcb18726d1b4dd1f3b9f360f49eb9e03ed317
DESKTOP-EBRN4UM\Tester <lazedays@naver.com>
2017-09-08 16:11:28 +0900

    cUiButton 클래스, cUiTextView 클래스 생성

---
+++ DirectX_Frame/DirectX_Frame/cUIButton.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "cUiObject.h"
+
+class cUIButton;
+
+class iButtonDelegate
+{
+public:
+//	virtual void OnClick(cUiObject* pSender) = 0;
+//	virtual void OnClick(cUiObject* pSender) {};
+};
+
+class cUIButton : public cUiObject
+{
+private:
+
+protected:
+
+public:
+
+	cUIButton(void);
+	virtual ~cUIButton(void);
+};
