99f0a221f3bc005b835a94b87a09fe8e7749fcf1
DESKTOP-EBRN4UM\Tester <lazedays@naver.com>
2017-09-10 21:40:34 +0900

    미완성 주석 처리

--- DirectX_Frame/DirectX_Frame/cUIImageView.h
+++ DirectX_Frame/DirectX_Frame/cUIImageView.h
@@ -7,7 +7,7 @@ private:
 	std::string m_sTexture;
 
 public:
-	void SetTexture(std::string sFullPath);
+//	void SetTexture(std::string sFullPath);
 //	void Render(LPD3DXSPRITE pSprite);
 
 	cUIImageView(void);
--- DirectX_Frame/DirectX_Frame/cUiTestScene.cpp
+++ DirectX_Frame/DirectX_Frame/cUiTestScene.cpp
@@ -37,7 +37,7 @@ HRESULT cUiTestScene::Setup(void)
 	D3DXCreateSprite(g_pD3DDevice, &m_pSprite);
 
 	cUIImageView* pimageView = new cUIImageView;
-	pimageView->SetTexture("Ui/panel-info.png");
+//	pimageView->SetTexture("Ui/panel-info.png");
 	m_pUiRoot = pimageView;
 
 	cUITextView* pTextView = new cUITextView;
