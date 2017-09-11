34c7f14ba3ab48dc5a742d319206085e6ebba38c
cafealpha <cyanine@naver.com>
2017-09-11 12:30:46 +0900

    포지션 변수 매크로 해제

--- DirectX_Frame/DirectX_Frame/cSkinnedMesh.h
+++ DirectX_Frame/DirectX_Frame/cSkinnedMesh.h
@@ -16,12 +16,19 @@ private:
 	int					m_nAnimationKey;
 	bool				m_isCurrentTrack;
 
+	//객체마다 생성
+	LPD3DXANIMATIONCONTROLLER	m_pAnimController;
 
+	D3DXVECTOR3 m_vPosition;
+	//SYNTHESIZE(ST_SPHERE, m_stSphere, BoundingSphere);
 
 
 public:
 	cSkinnedMesh(char* szFolder, char* szFilename);
 	~cSkinnedMesh(void);
+
+	D3DXVECTOR3 getPosition() { return m_vPosition; };
+	void setPosition(D3DXVECTOR3 value) { m_vPosition = value; };
 
 private:
 	cSkinnedMesh();
