c06725f57bf4f9bf95099e0350ec61c86c170bbe
박민호 <cafealpha@live.com>
2017-09-12 03:28:18 +0900

    소멸자 업데이트 함수 작성

--- DirectX_Frame/DirectX_Frame/cCharTestScene.cpp
+++ DirectX_Frame/DirectX_Frame/cCharTestScene.cpp
@@ -30,10 +30,18 @@ HRESULT cCharTestScene::Setup(void)
 
 void cCharTestScene::Reset(void)
 {
+	SAFE_RELEASE(m_pCamera);
+	//테스트용
+	SAFE_RELEASE(m_pGrid);
 }
 
 void cCharTestScene::Update(void)
 {
+	//테스트용
+	m_pCamera->TestController();
+
+	m_pCamera->Update();
+
 }
 
 void cCharTestScene::Render(void)
