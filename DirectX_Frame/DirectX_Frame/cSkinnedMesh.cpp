d524afaa1d5e1a66083c30f66f035d68ec6a4a17
cafealpha <cyanine@naver.com>
2017-09-11 12:38:46 +0900

    기본 생성자 구성

--- DirectX_Frame/DirectX_Frame/cSkinnedMesh.cpp
+++ DirectX_Frame/DirectX_Frame/cSkinnedMesh.cpp
@@ -1,3 +1,16 @@
 #include "StdAfx.h"
 #include "cSkinnedMesh.h"
 #include "cAllocateHierarchy.h"
+
+cSkinnedMesh::cSkinnedMesh(char * szFolder, char * szFilename)
+{
+}
+
+cSkinnedMesh::cSkinnedMesh()
+{
+}
+
+
+cSkinnedMesh::~cSkinnedMesh(void)
+{
+}
