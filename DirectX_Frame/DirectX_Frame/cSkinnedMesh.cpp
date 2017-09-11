7f6cdf8e782026bb95edc61e24779532badc6e14
cafealpha <cyanine@naver.com>
2017-09-11 12:39:27 +0900

    생성자 초기화

--- DirectX_Frame/DirectX_Frame/cSkinnedMesh.cpp
+++ DirectX_Frame/DirectX_Frame/cSkinnedMesh.cpp
@@ -3,10 +3,21 @@
 #include "cAllocateHierarchy.h"
 
 cSkinnedMesh::cSkinnedMesh(char * szFolder, char * szFilename)
+	: m_pRootFrame(NULL)
+	, m_pAnimController(NULL)
+	, m_dwWorkingPaletteSize(0)
+	, m_pmWorkingPalette(NULL)
+	, m_pEffect(NULL)
+	, m_vPosition(0, 0, 0)
 {
 }
 
 cSkinnedMesh::cSkinnedMesh()
+	: m_pRootFrame(NULL)
+	, m_pAnimController(NULL)
+	, m_dwWorkingPaletteSize(0)
+	, m_pmWorkingPalette(NULL)
+	, m_pEffect(NULL)
 {
 }
 
