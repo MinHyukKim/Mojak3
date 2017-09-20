5cd0d9b8a0dad7b9b5d302a0363fbdb89a4a92b6
cafealpha <cyanine@naver.com>
2017-09-20 21:35:39 +0900

    플레이어에 상태 설정 변수 삽입

--- DirectX_Frame/DirectX_Frame/cPlayer.h
+++ DirectX_Frame/DirectX_Frame/cPlayer.h
@@ -46,6 +46,11 @@ private:
 	int					m_nAnimationKey;
 	bool m_bCurrentTrack;
 
+	int m_state = ANIMATION_IDLE;
+	int m_prevState = m_state;
+	DWORD prevIndex = ANIMATION_IDLE;
+
+
 public:
 	virtual HRESULT Setup(void) override;
 	void Reset(void);
@@ -71,6 +76,8 @@ public:
 	void SetTextureHairColor(D3DMATERIAL9* pMaterial);
 	void SetTextureHairColor(LPD3DXCOLOR pColor);
 	void SetBlendingAnimation(int nAnimationKey, float fTravelTime = 0.1f);
+	void SetState(int nState) { m_prevState = m_state; m_state = nState; };
+	int GetState() { return m_state; };
 
 	cCamera* GetCamera(void) { return m_pCamera; }
 
