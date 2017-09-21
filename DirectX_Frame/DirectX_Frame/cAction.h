e7c217fc94f5a4cfc6848e7abe79f7d0e2944d55
NOTEBOOK\MinHyuk <rlaalsgur05@naver.com>
2017-09-21 16:39:07 +0900

    Action 클래스들 추가

---
+++ DirectX_Frame/DirectX_Frame/cAction.h
@@ -0,0 +1,44 @@
+#pragma once
+
+class cObject;
+class cAction;
+
+//액션이 끝난후에 진행할 액션
+class iActionDelegate
+{
+public:
+	virtual void OnFinish(cAction* pSender) = 0;
+};
+
+//액션 타이머
+class cAction : public cObject
+{
+private:
+	float m_fElapsedTime;
+	float m_fActionTime;
+	cObject* m_pObjectTarget;
+	iActionDelegate* m_pDelegate;
+
+public:
+	virtual void Play(void);
+	virtual void Update(void);
+
+	virtual void SetElapsedTime(float fElapsedTime) final { m_fElapsedTime = fElapsedTime; }
+	virtual float GetElapseTime(void) final { return m_fElapsedTime; }
+	virtual void SetMoveTo(void) { m_fElapsedTime = m_fActionTime; }
+	virtual void SetActionTime(float fActionTime) final { m_fActionTime = fActionTime; }
+	virtual float GetActionTime(void) final { return m_fActionTime; }
+	virtual void SetObjectTarget(cObject* pTarget) final { m_pObjectTarget = pTarget; }
+	virtual cObject* GetObjectTarget(void) final { return m_pObjectTarget; }
+	virtual void SetDelegate(iActionDelegate* pDelegate) final { m_pDelegate = pDelegate; }
+	virtual iActionDelegate* GetDelegate(void) final { return m_pDelegate; }
+
+	virtual float GetTravelTime(void) final { return m_fElapsedTime / m_fActionTime; }
+	virtual bool IsPlay(void) { return (m_fElapsedTime / m_fActionTime) < 1.0f; }
+
+	static cAction* Create(void);
+protected:
+	cAction(void);
+	virtual ~cAction(void);
+};
+
---
+++ DirectX_Frame/DirectX_Frame/cActionMove.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "cActionMove.h"
+#include "cObject.h"
+
+cActionMove::cActionMove(void)
+	: m_vFrom(D3DXVECTOR3(0.0f, 0.0f, 0.0f))
+	, m_vTo(D3DXVECTOR3(0.0f, 0.0f, 0.0f))
+{
+}
+
+cActionMove::~cActionMove(void)
+{
+}
+
+void cActionMove::Play(void)
+{
+	cObject* pTarget = this->GetObjectTarget();
+	if (pTarget)
+	{
+		pTarget->SetPosition(m_vFrom);
+	}
+	this->cAction::Play();
+}
+
+void cActionMove::Update(void)
+{
+	cObject* pTarget = this->GetObjectTarget();
+	if (!pTarget) return;
+	this->cAction::Update();
+	if (this->GetElapseTime() < this->GetActionTime())
+	{
+		float fTravelTime = this->GetElapseTime() / this->GetActionTime();
+
+		D3DXVECTOR3 vPosition;
+		D3DXVec3Lerp(&vPosition, &m_vFrom, &m_vTo, this->GetTravelTime());
+		pTarget->SetPosition(vPosition);
+	}
+	else
+	{
+		pTarget->SetPosition(m_vTo);
+
+		iActionDelegate* pDelegate = this->GetDelegate();
+		if (pDelegate)
+		{
+			pDelegate->OnFinish(this);
+		}
+	}
+}
+
+cActionMove* cActionMove::AddRef(void)
+{
+	return (cActionMove*) this->cNode::AddRef();
+}
+
+cActionMove* cActionMove::Create(void)
+{
+	cActionMove* newAction = new cActionMove;
+	return (cActionMove*)newAction->AutoRelease();
+}
+
