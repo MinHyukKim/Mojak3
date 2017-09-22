00d6de7ff87cb7c516a2e212b6ec1e7d66b16e3b
cafealpha <cyanine@naver.com>
2017-09-22 12:15:18 +0900

    빌딩 클래스 구현 자리 만듬

--- DirectX_Frame/DirectX_Frame/cBuilding.cpp
+++ DirectX_Frame/DirectX_Frame/cBuilding.cpp
@@ -1,10 +1,30 @@
 #include "stdafx.h"
 #include "cBuilding.h"
 
+HRESULT cBuilding::Setup(void)
+{
+	return E_NOTIMPL;
+}
+
+void cBuilding::Reset(void)
+{
+}
+
+void cBuilding::Update(void)
+{
+}
+
+void cBuilding::Render(void)
+{
+}
+
 cBuilding::cBuilding(void)
 {
+	D3DXMatrixIdentity(&m_matWorld);
 }
 
 cBuilding::~cBuilding(void)
 {
+	this->Reset();
+
 }
